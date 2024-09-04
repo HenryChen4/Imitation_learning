bf91deded2f1c4b85f808420bd46ff82e11c0963
Henry Chen <hchen365@usc.edu>
2024-09-04 00:47:50 -0700

    testing

--- src/training.py
+++ src/training.py
@@ -2,6 +2,7 @@
 import torch.nn as nn
 import torch
 import numpy as np
+from tqdm import tqdm, trange
 
 from src.mlp_actor import MLPActor
 from src.simulate import rollout
@@ -11,34 +12,40 @@ from ribs.emitters.opt import CMAEvolutionStrategy
 def train(params,
           actor_config,
           num_iters,
+          top_prop=4,
           sigma0=0.1,
           batch_size=30,
           seed=135135):
-    # initialize evolution strategy operator
     solution_dim = params.shape[0]
     evolution_operator = CMAEvolutionStrategy(sigma0=sigma0,
                                               batch_size=batch_size,
                                               solution_dim=solution_dim,
                                               seed=seed)
-
-    print(solution_dim)
-
     initial_mean = np.mean(params) 
     evolution_operator.reset(initial_mean)
     
-    for i in range(num_iters):
-        # sample new parameters and give them to model
+    print("> Beginning black box search")
+    for i in trange(num_iters):
         sols = evolution_operator.ask()
 
-        # TODO: Use DASK eventually for multithreading
+        # TODO: Use DASK for multithreading
+        sols_rewards = []
         for sol_i in sols:
             actor = MLPActor(**actor_config)
             actor.deserialize(sol_i)
+            _, sol_i_reward = rollout(actor_config=actor_config,
+                                      params=sol_i)
+            sols_rewards.append(sol_i_reward)
+        
+        ranking_indices = np.flip(np.argsort(sols_rewards))
+        ranking_values = sols_rewards
+ 
+        num_parents = len(sols_rewards//top_prop)
 
-            # retrieve rewards of actor
-            _, rewards = rollout(actor_config=actor_config,
-                                 params=sol_i)
-            print(rewards)
+        evolution_operator.tell(ranking_indices,
+                                ranking_values,
+                                num_parents)
+        print(max(sols_rewards))
 
 actor_config = {
     "layer_shapes": [(4, 128),
